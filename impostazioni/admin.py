08a002e1970cf34d1e5069c60d98264f2f70beb8
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-05-19 19:03:21 +0200

    test

--- impostazioni/admin.py
+++ impostazioni/admin.py
@@ -3,6 +3,8 @@ from solo.admin import SingletonModelAdmin
 from .models import GeneraliModel
 
 # Register your models here.
+
+
 @admin.register(GeneraliModel)
 class AdminGeneraliModel(SingletonModelAdmin):
     pass
