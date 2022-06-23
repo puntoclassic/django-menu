8d62a8d7d356bd0d990fb94a7901c280ab651b1d
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-06-23 16:20:31 +0200

    header url

--- commerce/urls.py
+++ commerce/urls.py
@@ -3,15 +3,15 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('Categoria/<slug:slug>', CategoriaListView.as_view(), name='category-show'),
-    path('Errore', ErrorPageView.as_view(), name='error-page'),
-    path('Carrello', CartView.as_view(), name='show-cart'),
-    path('Carrello/AggiungiAlCarrello',
+    path('categoria/<slug:slug>', CategoriaListView.as_view(), name='category-show'),
+    path('errore', ErrorPageView.as_view(), name='error-page'),
+    path('carrello', CartView.as_view(), name='show-cart'),
+    path('carrello/aggiungiAlCarrello',
          AddToCartView.as_view(), name='add-to-cart'),
-    path('Carrello/RimuoviDalCarrello',
+    path('carrello/rimuoviDalCarrello',
          RemoveFromCartView.as_view(), name='remove-from-cart'),
-    path('Carrello/IncrementaQuantità',
+    path('carrello/incrementaQuantità',
          IncreaseQtyView.as_view(), name='increase-qty'),
-    path('Carrello/RiduciQuantità',
+    path('carrello/riduciQuantità',
          DecreaseQtyView.as_view(), name='decrease-qty'),
 ]
