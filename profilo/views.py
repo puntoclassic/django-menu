6ae9431c73e7278b29e05b404ff29c5f185e00d8
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-06-28 19:39:08 +0200

    password reset email template change

--- profilo/views.py
+++ profilo/views.py
@@ -9,65 +9,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
 from allauth.account.views import LoginView
 from .models import User
-# Create your views here.
-
-'''
-class CustomLoginView(LoginView):
-    form_class = CustomLoginForm
-    redirect_url = reverse_lazy('account')
-
-class CustomSignInView(CreateView):
-    template_name = "profilo/signin.html"
-    form_class = CustomSignInForm
-    success_url = reverse_lazy('account_login') '''
-
-
-
-class CustomPasswordResetView(PasswordResetView):
-    form_class = CustomPasswordRecoveryForm
-    subject_template_name = "account/email/recupera-password-subject.html"
-    email_template_name = "account/email/recupera-password-body.html"
-    html_email_template_name = "account/email/recupera-password-body.html"
-    template_name = "profilo/recupera-password/recupera-password-1.html"
-
-class CustomPasswordResetDone(PasswordResetDoneView):
-    template_name = "profilo/recupera-password/recupera-password-2.html"
-
-class CustomPasswordResetConfirmView(PasswordResetConfirmView):
-    template_name = "profilo/recupera-password/recupera-password-3.html"
-
-class CustomPasswordResetCompleted(PasswordChangeDoneView):
-    template_name = "profilo/recupera-password/recupera-password-4.html"
 
 class ProfiloView(LoginRequiredMixin, TemplateView):
-    template_name = "profilo/index.html"
+    template_name = "account/index.html"
     redirect_field_name = 'redirect_to'
 
-class AccountInviaMessaggio(LoginRequiredMixin, FormView):
-    template_name = 'form-contatto/form-contatto-1.html'
-    form_class = ContactForm
-    success_url = reverse_lazy('invia-messaggio-ok')
-
-    def get(self, request, *args, **kwargs):
-        self.initial["first_name"] = request.user.first_name
-        self.initial["last_name"] = request.user.last_name
-        self.initial["email"] = request.user.email
-
-        return super().get(request, *args, **kwargs)
-
-    def form_valid(self, form):
-        return super().form_valid(form)
-
-class AccountInviaMessaggioDone(LoginRequiredMixin, TemplateView):
-    template_name = 'form-contatto/form-contatto-2.html'
-
 class AccountInformazioniProfiloView(TemplateView):
-    template_name = "profilo/informazioni-profilo/view.html"
+    template_name = "account/informazioni-profilo/view.html"
 
 class AccountInformazioniProfiloEdit(UpdateView):
     model = User
     form_class = AccountInformazioniEditForm
-    template_name = "profilo/informazioni-profilo/edit.html"
+    template_name = "account/informazioni-profilo/edit.html"
 
     def form_valid(self, form):
         messages.success(
