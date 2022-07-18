284aa13d29dc9ed7da91ada7b9d902a07734460c
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-07-18 16:43:45 +0200

    url scaffolding

--- shop/settings.py
+++ shop/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'profilo'
+LOGIN_REDIRECT_URL = 'impostazioni.profilo.index'
 AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
